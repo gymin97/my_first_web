d911163557a42cf9144086382f6029c2e1406643
GyeongMin Kim <gymin97@gmail.com>
2022-01-18 00:30:55 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*#ii(et^wu*e1l)27(_m0_u86tt4$#2uq2uz)x*or-^sqlhq)v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
